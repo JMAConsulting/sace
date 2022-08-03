5d2f997dc4ba4fcdc561647f234b74b024e16d02
Seamus Lee <seamus.lee@jmaconsulting.biz>
2022-08-03 03:04:05 -0400

    Add in CDN Tax Receipts Extension

---
+++ web/sites/default/files/civicrm/ext/org.civicrm.cdntaxreceipts/CRM/Cdntaxreceipts/Form/Settings.php
@@ -0,0 +1,244 @@
+<?php
+
+require_once 'CRM/Core/Form.php';
+require_once 'cdntaxreceipts.functions.inc';
+
+/**
+ * Form controller class
+ *
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
+ */
+class CRM_Cdntaxreceipts_Form_Settings extends CRM_Core_Form {
+
+  CONST SETTINGS = 'CDNTaxReceipts';
+
+  protected $unsavedChangesWarn = TRUE;
+
+  function buildQuickForm() {
+
+    CRM_Utils_System::setTitle(ts('Configure CDN Tax Receipts', array('domain' => 'org.civicrm.cdntaxreceipts')));
+    CRM_Core_Resources::singleton()->addStyleFile('org.civicrm.cdntaxreceipts', 'css/civicrm_cdntaxreceipts.css');
+
+    $this->processOrgOptions('build');
+    $this->processReceiptOptions('build');
+    $this->processSystemOptions('build');
+    $this->processEmailOptions('build');
+
+    $arr1 = $this->processOrgOptions('defaults');
+    $arr2 = $this->processReceiptOptions('defaults');
+    $arr3 = $this->processSystemOptions('defaults');
+    $arr4 = $this->processEmailOptions('defaults');
+    $defaults = array_merge($arr1, $arr2, $arr3, $arr4);
+    $this->setDefaults($defaults);
+
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => ts('Submit', array('domain' => 'org.civicrm.cdntaxreceipts')),
+        'isDefault' => TRUE,
+      ),
+    ));
+    // Set image defaults
+    $images = array('receipt_logo', 'receipt_signature', 'receipt_watermark', 'receipt_pdftemplate');
+    foreach ($images as $image) {
+      if (!empty($defaults[$image])) {
+        $this->assign($image, $defaults[$image]);
+        if (!file_exists(CRM_Core_Config::singleton()->customFileUploadDir . $defaults[$image])) {
+          $this->assign($image.'_class', TRUE);
+        }
+      }
+    }
+
+    parent::buildQuickForm();
+  }
+
+  function processOrgOptions($mode) {
+    if ( $mode == 'build' ) {
+      $this->add('text', 'org_name', ts('Organization Name', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'org_address_line1', ts('Address Line 1', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'org_address_line2', ts('Address Line 2', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'org_tel', ts('Telephone', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'org_fax', ts('Fax', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'org_email', ts('Email', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'org_web', ts('Website', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'org_charitable_no', ts('Charitable Registration Number', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'receipt_location_issued', ts('Location Issued', array('domain' => 'org.civicrm.cdntaxreceipts')));
+
+      // @todo: Do we need to do this - can't we just use the `required` param in add() above?
+      // If we do need to do this, shouldn't the labels be ts'd?
+      $this->addRule('org_name', 'Enter Organization Name', 'required');
+      $this->addRule('org_address_line1', 'Enter Address Line 1', 'required');
+      $this->addRule('org_address_line2', 'Enter Address Line 2', 'required');
+      $this->addRule('org_tel', 'Enter Telephone', 'required');
+      $this->addRule('org_email', 'Enter Email', 'required');
+      $this->addRule('org_web', 'Enter Website', 'required');
+      $this->addRule('org_charitable_no', 'Enter Charitable Number', 'required');
+      $this->addRule('receipt_location_issued', 'Location Issued', 'required');
+    }
+    else if ( $mode == 'defaults' ) {
+      $defaults = array(
+        'org_name' => Civi::settings()->get('org_name'),
+        'org_address_line1' => Civi::settings()->get('org_address_line1'),
+        'org_address_line2' => Civi::settings()->get('org_address_line2'),
+        'org_tel' => Civi::settings()->get('org_tel'),
+        'org_fax' => Civi::settings()->get('org_fax'),
+        'org_email' => Civi::settings()->get('org_email'),
+        'org_web' => Civi::settings()->get('org_web'),
+        'receipt_logo' => Civi::settings()->get('receipt_logo'),
+        'receipt_signature' => Civi::settings()->get('receipt_signature'),
+        'receipt_watermark' => Civi::settings()->get('receipt_watermark'),
+        'receipt_pdftemplate' => Civi::settings()->get('receipt_pdftemplate'),
+        'org_charitable_no' => Civi::settings()->get('org_charitable_no'),
+        'receipt_location_issued' => Civi::settings()->get('receipt_location_issued'),
+      );
+      return $defaults;
+    }
+    else if ( $mode == 'post' ) {
+      $values = $this->exportValues();
+      Civi::settings()->set('org_name', $values['org_name']);
+      Civi::settings()->set('org_address_line1', $values['org_address_line1']);
+      Civi::settings()->set('org_address_line2', $values['org_address_line2']);
+      Civi::settings()->set('org_tel', $values['org_tel']);
+      Civi::settings()->set('org_fax', $values['org_fax']);
+      Civi::settings()->set('org_email', $values['org_email']);
+      Civi::settings()->set('org_web', $values['org_web']);
+      Civi::settings()->set('org_charitable_no', $values['org_charitable_no']);
+      Civi::settings()->set('receipt_location_issued', $values['receipt_location_issued']);
+    }
+
+  }
+
+  function processReceiptOptions($mode) {
+    if ( $mode == 'build' ) {
+      $this->add('text', 'receipt_prefix', ts('Receipt Prefix', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'receipt_authorized_signature_text', ts('Authorized Signature Text', array('domain' => 'org.civicrm.cdntaxreceipts')));
+
+      $uploadSize = cdntaxreceipts_getCiviSetting('maxFileSize');
+      if ($uploadSize >= 8 ) {
+        $uploadSize = 8;
+      }
+      $uploadFileSize = $uploadSize * 1024 * 1024;
+
+      $this->assign('uploadSize', $uploadSize );
+      $this->setMaxFileSize( $uploadFileSize );
+
+      $this->addElement('file', 'receipt_logo', ts('Organization Logo', array('domain' => 'org.civicrm.cdntaxreceipts')), 'size=30 maxlength=60');
+      $this->addUploadElement('receipt_logo');
+      $this->addRule( 'receipt_logo', ts('File size should be less than %1 MBytes (%2 bytes)', array(1 => $uploadSize, 2 => $uploadFileSize)), 'maxfilesize', $uploadFileSize, array('domain' => 'org.civicrm.cdntaxreceipts') );
+
+      $this->addElement('file', 'receipt_signature', ts('Signature Image', array('domain' => 'org.civicrm.cdntaxreceipts')), 'size=30 maxlength=60');
+      $this->addUploadElement('receipt_signature');
+      $this->addRule( 'receipt_signature', ts('File size should be less than %1 MBytes (%2 bytes)', array(1 => $uploadSize, 2 => $uploadFileSize)), 'maxfilesize', $uploadFileSize, array('domain' => 'org.civicrm.cdntaxreceipts') );
+
+      $this->addElement('file', 'receipt_watermark', ts('Watermark Image', array('domain' => 'org.civicrm.cdntaxreceipts')), 'size=30 maxlength=60');
+      $this->addUploadElement('receipt_watermark');
+      $this->addRule( 'receipt_watermark', ts('File size should be less than %1 MBytes (%2 bytes)', array(1 => $uploadSize, 2 => $uploadFileSize)), 'maxfilesize', $uploadFileSize, array('domain' => 'org.civicrm.cdntaxreceipts') );
+
+      $this->addElement('file', 'receipt_pdftemplate', ts('PDF Template', array('domain' => 'org.civicrm.cdntaxreceipts')), 'size=30 maxlength=60');
+      $this->addUploadElement('receipt_pdftemplate');
+      $this->addRule( 'receipt_pdftemplate', ts('File size should be less than %1 MBytes (%2 bytes)', array(1 => $uploadSize, 2 => $uploadFileSize)), 'maxfilesize', $uploadFileSize, array('domain' => 'org.civicrm.cdntaxreceipts') );
+    }
+    else if ( $mode == 'defaults' ) {
+      $defaults = array(
+        'receipt_prefix' => Civi::settings()->get('receipt_prefix'),
+        'receipt_authorized_signature_text' => Civi::settings()->get('receipt_authorized_signature_text'),
+      );
+      return $defaults;
+    }
+    else if ( $mode == 'post' ) {
+      $values = $this->exportValues();
+      Civi::settings()->set('receipt_prefix', $values['receipt_prefix']);
+      Civi::settings()->set('receipt_authorized_signature_text', $values['receipt_authorized_signature_text']);
+
+      foreach ( array('receipt_logo', 'receipt_signature', 'receipt_watermark', 'receipt_pdftemplate') as $key ) {
+        $upload_file = $this->getSubmitValue($key);
+        if (is_array($upload_file)) {
+          if ( $upload_file['error'] == 0 ) {
+            $filename = CRM_Utils_File::makeFileName($upload_file['name']);
+            if (!move_uploaded_file($upload_file['tmp_name'], CRM_Core_Config::singleton()->customFileUploadDir . $filename)) {
+              CRM_Core_Error::fatal(ts('Could not upload the file'));
+            }
+            Civi::settings()->set($key, $filename);
+          }
+        }
+      }
+    }
+  }
+
+  function processSystemOptions($mode) {
+    if ( $mode == 'build' ) {
+      $this->addElement('checkbox', 'issue_inkind', ts('Setup in-kind receipts?', array('domain' => 'org.civicrm.cdntaxreceipts')));
+
+      $delivery_options = array();
+      $delivery_options[] = $this->createElement('radio', NULL, NULL, 'Print only', CDNTAX_DELIVERY_PRINT_ONLY);
+      $delivery_options[] = $this->createElement('radio', NULL, NULL, 'Email or print', CDNTAX_DELIVERY_PRINT_EMAIL);
+      $delivery_options[] = $this->createElement('radio', NULL, NULL, 'Data only', CDNTAX_DELIVERY_DATA_ONLY);
+      $this->addGroup($delivery_options, 'delivery_method', ts('Delivery Method', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->addRule('delivery_method', 'Delivery Method', 'required');
+
+      $yesno_options = array();
+      $yesno_options[] = $this->createElement('radio', NULL, NULL, 'Yes', 1);
+      $yesno_options[] = $this->createElement('radio', NULL, NULL, 'No', 0);
+      $this->addGroup($yesno_options, 'attach_to_workflows', ts('Attach receipts to automated workflow messages?', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->addRule('attach_to_workflows', 'Attach tax receipts to automated messages', 'required');
+
+      $yesno_options2 = array();
+      $yesno_options2[] = $this->createElement('radio', NULL, NULL, 'Yes', 1);
+      $yesno_options2[] = $this->createElement('radio', NULL, NULL, 'No', 0);
+      $this->addGroup($yesno_options2, 'enable_advanced_eligibility_report', ts('Enable Advanced Eligibility Check?', array('domain' => 'org.civicrm.cdntaxreceipts')));
+    }
+    else if ( $mode == 'defaults' ) {
+      $defaults = array(
+        'issue_inkind' => 0,
+        'delivery_method' => Civi::settings()->get('delivery_method') ?? CDNTAX_DELIVERY_PRINT_ONLY,
+        'attach_to_workflows' => Civi::settings()->get('attach_to_workflows') ?? 0,
+        'enable_advanced_eligibility_report' => Civi::settings()->get('enable_advanced_eligibility_report') ?? 0,
+      );
+      return $defaults;
+    }
+    else if ( $mode == 'post' ) {
+      $values = $this->exportValues();
+      Civi::settings()->set('delivery_method', $values['delivery_method']);
+      Civi::settings()->set('attach_to_workflows', $values['attach_to_workflows']);
+      Civi::settings()->set('enable_advanced_eligibility_report', $values['enable_advanced_eligibility_report']);
+      if (isset($values['issue_inkind']) == TRUE) {
+        if ( $values['issue_inkind'] == 1 ) {
+          cdntaxreceipts_configure_inkind_fields();
+        }
+      }
+    }
+  }
+
+  function processEmailOptions($mode) {
+    if ( $mode == 'build' ) {
+      $this->add('text', 'email_from', ts('Email From', array('domain' => 'org.civicrm.cdntaxreceipts')));
+      $this->add('text', 'email_archive', ts('Archive Email', array('domain' => 'org.civicrm.cdntaxreceipts')));
+
+      $this->addRule('email_from', 'Enter email from address', 'required');
+      $this->addRule('email_archive', 'Enter email archive address', 'required');
+    }
+    else if ( $mode == 'defaults' ) {
+      $defaults = array(
+        'email_from' => Civi::settings()->get('email_from'),
+        'email_archive' => Civi::settings()->get('email_archive'),
+      );
+      return $defaults;
+    }
+    else if ( $mode == 'post' ) {
+      $values = $this->exportValues();
+      Civi::settings()->set('email_from', $values['email_from']);
+      Civi::settings()->set('email_archive', $values['email_archive']);
+    }
+  }
+
+  function postProcess() {
+    parent::postProcess();
+    $this->processOrgOptions('post');
+    $this->processReceiptOptions('post');
+    $this->processSystemOptions('post');
+    $this->processEmailOptions('post');
+
+    $statusMsg = ts('Your settings have been saved.', array('domain' => 'org.civicrm.cdntaxreceipts'));
+    CRM_Core_Session::setStatus( $statusMsg, '', 'success' );
+  }
+}
---
+++ web/sites/default/files/civicrm/ext/org.civicrm.cdntaxreceipts/CRM/Cdntaxreceipts/Upgrader.php
@@ -0,0 +1,299 @@
+<?php
+
+/**
+ * Collection of upgrade steps
+ */
+class CRM_Cdntaxreceipts_Upgrader extends CRM_Cdntaxreceipts_Upgrader_Base {
+
+  // By convention, functions that look like "function upgrade_NNNN()" are
+  // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
+
+  /**
+   * Example: Run an external SQL script when the module is installed.
+   */
+  public function install() {
+    $this->createTables();
+
+    $email_message = '{$contact.email_greeting_display},
+
+Attached please find your official tax receipt for income tax purposes.
+
+{$orgName}';
+    $email_subject = 'Your tax receipt {$receipt.receipt_no}';
+
+    $this->_create_message_template($email_message, $email_subject);
+  }
+
+  /**
+   * Example: Run an external SQL script when the module is uninstalled.
+   */
+  public function uninstall() {
+    $this->executeSqlFile('sql/uninstall.sql');
+  }
+
+  /**
+   * Get the character set and collation that the core CiviCRM tables are
+   * currently using.
+   * @return array
+   */
+  private function getDatabaseCharacterSettings():array {
+    $values = [
+      'charset' => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+    ];
+    // This doesn't exist before 5.29. Not worth implementing ourselves, just
+    // use defaults above.
+    if (method_exists('CRM_Core_BAO_SchemaHandler', 'getInUseCollation')) {
+      $values['collation'] = CRM_Core_BAO_SchemaHandler::getInUseCollation();
+      if (stripos($values['collation'], 'utf8mb4') !== FALSE) {
+        $values['charset'] = 'utf8mb4';
+      }
+    }
+    return $values;
+  }
+
+  /**
+   * Create the tables.
+   *
+   * changes made in:
+   *   0.9.beta1
+   *   1.5.4 - use same character set that core tables are currently using
+   *
+   * NOTE: We avoid direct foreign keys to CiviCRM schema because this log should
+   * remain intact even if a particular contact or contribution is deleted (for
+   * auditing purposes).
+   */
+  protected function createTables() {
+    $character_settings = $this->getDatabaseCharacterSettings();
+
+    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS cdntaxreceipts_log_contributions");
+    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS cdntaxreceipts_log");
+
+    CRM_Core_DAO::executeQuery("CREATE TABLE cdntaxreceipts_log (
+id int(11) NOT NULL AUTO_INCREMENT COMMENT 'The internal id of the issuance.',
+receipt_no varchar(128) NOT NULL  COMMENT 'Receipt Number.',
+issued_on int(11) NOT NULL COMMENT 'Unix timestamp of when the receipt was issued, or re-issued.',
+contact_id int(10) unsigned NOT NULL COMMENT 'CiviCRM contact id to whom the receipt is issued.',
+receipt_amount decimal(10,2) NOT NULL COMMENT 'Receiptable amount, total minus non-receiptable portion.',
+is_duplicate tinyint(4) NOT NULL COMMENT 'Boolean indicating whether this is a re-issue.',
+uid int(10) unsigned NOT NULL COMMENT 'Drupal user id of the person issuing the receipt.',
+ip varchar(128) NOT NULL COMMENT 'IP of the user who issued the receipt.',
+issue_type varchar(16) NOT NULL COMMENT 'The type of receipt (single or annual).',
+issue_method varchar(16) NULL COMMENT 'The send method (email or print).',
+receipt_status varchar(10) DEFAULT 'issued' COMMENT 'The status of the receipt (issued or cancelled)',
+email_tracking_id varchar(64) NULL COMMENT 'A unique id to track email opens.',
+email_opened datetime NULL COMMENT 'Timestamp an email open event was detected.',
+location_issued varchar(32) NOT NULL DEFAULT '' COMMENT 'City where receipt was issued.',
+PRIMARY KEY (id),
+INDEX contact_id (contact_id),
+INDEX receipt_no (receipt_no)
+) ENGINE=InnoDB DEFAULT CHARSET={$character_settings['charset']} COLLATE {$character_settings['collation']} COMMENT='Log file of tax receipt issuing.'");
+
+    // The contribution_id is *deliberately* not a foreign key to civicrm_contribution.
+    // We don't want to destroy audit records if contributions are deleted.
+    CRM_Core_DAO::executeQuery("CREATE TABLE cdntaxreceipts_log_contributions (
+id int(11) NOT NULL AUTO_INCREMENT COMMENT 'The internal id of this line.',
+receipt_id int(11) NOT NULL COMMENT 'The internal receipt ID this line belongs to.',
+contribution_id int(10) unsigned NOT NULL COMMENT 'CiviCRM contribution id for which the receipt is issued.',
+contribution_amount decimal(10,2) DEFAULT NULL COMMENT 'Total contribution amount.',
+receipt_amount decimal(10,2) NOT NULL COMMENT 'Receiptable amount, total minus non-receiptable portion.',
+receive_date datetime NOT NULL COMMENT 'Date on which the contribution was received, redundant information!',
+PRIMARY KEY (id),
+FOREIGN KEY (receipt_id) REFERENCES cdntaxreceipts_log(id),
+INDEX contribution_id (contribution_id)
+) ENGINE=InnoDB DEFAULT CHARSET={$character_settings['charset']} COLLATE {$character_settings['collation']} COMMENT='Contributions for each tax reciept issuing.'");
+  }
+
+  /**
+   * @TODO This function is buggy - it returns false when the field already
+   * exists. Also the entire function could just be replaced with CRM_Upgrade...addColumn().
+   */
+  public function upgrade_1320() {
+    $this->ctx->log->info('Applying update 1.3.2');
+    $dao =& CRM_Core_DAO::executeQuery("SELECT 1");
+    $db_name = $dao->_database;
+    $dao =& CRM_Core_DAO::executeQuery("
+SELECT COUNT(*) as col_count
+FROM information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = '{$db_name}'
+AND TABLE_NAME = 'cdntaxreceipts_log'
+AND COLUMN_NAME = 'receipt_status'");
+    if ($dao->fetch()) {
+      if ($dao->col_count == 0) {
+        CRM_Core_DAO::executeQuery("ALTER TABLE cdntaxreceipts_log ADD COLUMN receipt_status varchar(10) DEFAULT 'issued'");
+        $ndao =& CRM_Core_DAO::executeQuery("
+SELECT COUNT(*) as col_count
+FROM information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = '{$db_name}'
+AND TABLE_NAME = 'cdntaxreceipts_log'
+AND COLUMN_NAME = 'receipt_status'");
+        if ($ndao->fetch()) {
+          if ($ndao->col_count == 1) {
+            return TRUE;
+          }
+        }
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * @TODO replace with CRM_Upgrade...addColumn and also there's one called
+   * safeIndex() or something like that.
+   */
+  public function upgrade_1321() {
+    $this->ctx->log->info('Applying update 1321: Email Tracking');
+    CRM_Core_DAO::executeQuery('ALTER TABLE cdntaxreceipts_log ADD email_tracking_id varchar(64) NULL');
+    CRM_Core_DAO::executeQuery('ALTER TABLE cdntaxreceipts_log ADD email_opened datetime NULL');
+    CRM_Core_DAO::executeQuery('CREATE INDEX contribution_id ON cdntaxreceipts_log_contributions (contribution_id)');
+    return TRUE;
+  }
+
+  public function upgrade_1322() {
+    $this->ctx->log->info('Applying update 1322: Message Templates');
+    $current_message = Civi::settings()->get('email_message');
+    $current_subject = Civi::settings()->get('email_subject') . ' {$receipt.receipt_no}';
+    return $this->_create_message_template($current_message, $current_subject);
+  }
+
+  public function upgrade_1410() {
+    $this->ctx->log->info('Applying update 1410: Data mode');
+    $email_enabled = Civi::settings()->get('enable_email');
+    if ($email_enabled) {
+      Civi::settings()->set('delivery_method', 1);
+    }
+    else {
+      Civi::settings()->set('delivery_method', 0);
+    }
+    return TRUE;
+  }
+
+  /**
+   * Update uploaded file paths to be relative instead of absolute.
+   */
+  public function upgrade_1411() {
+    $this->ctx->log->info('Applying update 1411: uploaded file paths');
+    foreach (array('receipt_logo', 'receipt_signature', 'receipt_watermark', 'receipt_pdftemplate') as $fileSettingName) {
+      $path = Civi::settings()->get($fileSettingName);
+      if (!empty($path)) {
+        Civi::settings()->set($fileSettingName, basename($path));
+      }
+    }
+    return TRUE;
+  }
+
+  /**
+   * Add location issued column
+   */
+  public function upgrade_1412() {
+    $this->ctx->log->info('Applying update 1412: add location issued column');
+    // We don't extend the incremental base class, so we can't add a task and need to call directly.
+    CRM_Upgrade_Incremental_Base::addColumn($this->ctx, 'cdntaxreceipts_log', 'location_issued', "varchar(32) NOT NULL DEFAULT '' COMMENT 'City where receipt was issued.'");
+    return TRUE;
+  }
+
+  public function _create_message_template($email_message, $email_subject) {
+
+    $html_message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+ <title></title>
+</head>
+<body>
+{capture assign=headerStyle}colspan="2" style="text-align: left; padding: 4px; border-bottom: 1px solid #999; background-color: #eee;"{/capture}
+{capture assign=labelStyle }style="padding: 4px; border-bottom: 1px solid #999; background-color: #f7f7f7;"{/capture}
+{capture assign=valueStyle }style="padding: 4px; border-bottom: 1px solid #999;"{/capture}
+
+<center>
+ <table width="620" border="0" cellpadding="0" cellspacing="0" style="font-family: Arial, Verdana, sans-serif; text-align: left;">
+
+  <!-- BEGIN HEADER -->
+  <!-- You can add table row(s) here with logo or other header elements -->
+  <!-- END HEADER -->
+
+  <!-- BEGIN CONTENT -->
+
+  <tr>
+   <td>
+    <p>' . nl2br(htmlspecialchars($email_message)) . '</p>
+   </td>
+  </tr>
+  <tr>
+ </table>
+</center>
+{$openTracking}
+</body>
+</html>';
+
+    // create message template for email that accompanies tax receipts
+    $params = array(
+      'sequential' => 1,
+      'name' => 'msg_tpl_workflow_cdntaxreceipts',
+      'title' => 'Message Template Workflow for CDN Tax Receipts',
+      'description' => 'Message Template Workflow for CDN Tax Receipts',
+      'is_reserved' => 1,
+      'is_active' => 1,
+      'api.OptionValue.create' => array(
+        '0' => array(
+          'label' => 'CDN Tax Receipts - Email Single Receipt',
+          'value' => 1,
+          'name' => 'cdntaxreceipts_receipt_single',
+          'is_reserved' => 1,
+          'is_active' => 1,
+          'format.only_id' => 1,
+        ),
+        '1' => array(
+          'label' => 'CDN Tax Receipts - Email Annual/Aggregate Receipt',
+          'value' => 2,
+          'name' => 'cdntaxreceipts_receipt_aggregate',
+          'is_reserved' => 1,
+          'is_active' => 1,
+          'format.only_id' => 1,
+        ),
+      ),
+    );
+    $result = civicrm_api3('OptionGroup', 'create', $params);
+
+    $params = array(
+      'msg_title' => 'CDN Tax Receipts - Email Single Receipt',
+      'msg_subject' => $email_subject,
+      'msg_text' => $email_message,
+      'msg_html' => $html_message,
+      'workflow_id' => $result['values'][0]['api.OptionValue.create'][0],
+      'is_default' => 1,
+      'is_reserved' => 0,
+    );
+    civicrm_api3('MessageTemplate', 'create', $params);
+
+    $params = array(
+      'msg_title' => 'CDN Tax Receipts - Email Annual/Aggregate Receipt',
+      'msg_subject' => $email_subject,
+      'msg_text' => $email_message,
+      'msg_html' => $html_message,
+      'workflow_id' => $result['values'][0]['api.OptionValue.create'][1],
+      'is_default' => 1,
+      'is_reserved' => 0,
+    );
+    civicrm_api3('MessageTemplate', 'create', $params);
+
+    return TRUE;
+  }
+
+  /**
+   * Example: Run an external SQL script
+   *
+   * @return TRUE on success
+   * @throws Exception
+   */
+  /*
+  public function upgrade_4201() {
+  $this->ctx->log->info('Applying update 4201');
+  // this path is relative to the extension base dir
+  $this->executeSqlFile('sql/upgrade_4201.sql');
+  return TRUE;
+  } */
+
+}
