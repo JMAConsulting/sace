713d79600a52b6ba404edaca4b737f08a4ebf36d
Kashish Lathidaida~ <kashish.lathidadia@jmaconsulting.biz>
2022-12-07 22:43:46 -0500

    initial

---
+++ web/sites/default/files/civicrm/ext/userteamactivity/Civi/Api4/UserTeamActivityEntity.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * UserTeamActivityEntity entity.
+ *
+ * Provided by the userteamactivity extension.
+ *
+ * @package Civi\Api4
+ */
+class UserTeamActivityEntity extends Generic\DAOEntity {
+
+}
