09b2a80d8262c4b48e88487365e7e36d5d5a94ba
Seamus Lee <seamuslee001@gmail.com>
2023-01-03 11:32:51 +1100

    Exclude logging and also exclude finding source contact records in activity contacts as we reset that

--- web/sites/default/modules/sace_custom/src/Plugin/WebformHandler/SaceUpdateActivityWebformHandler.php
+++ web/sites/default/modules/sace_custom/src/Plugin/WebformHandler/SaceUpdateActivityWebformHandler.php
@@ -55,20 +55,19 @@ class SaceUpdateActivityWebformHandler extends WebformHandlerBase {
     $civicrm_submission_data = $this->database->query("SELECT civicrm_data FROM {webform_civicrm_submissions} WHERE sid = :sid", [
       ':sid' => $webform_submission->id(),
     ]);
-\CRM_Core_Error::debug_var('a', $civicrm_submission_data);
 
     if ($civicrm_submission_data && !empty($webform_submission_data['rescheduled_activity_date'])) {
       while ($row = $civicrm_submission_data->fetchAssoc()) {
         $data = unserialize($row['civicrm_data']);
         $activity = Activity::get(FALSE)->addWhere('id', '=', $data['activity'][1]['id'])->execute()[0];
-        $activityContacts = ActivityContact::get(FALSE)->addWhere('activity_id', '=', $data['activity'][1]['id'])->execute();
+        $activityContacts = ActivityContact::get(FALSE)->addWhere('activity_id', '=', $data['activity'][1]['id'])->addWhere('record_type_id', '!=', 2)->execute();
         unset($activity['id']);
         $activity['activity_date_time'] = $webform_submission_data['rescheduled_activity_date'];
         $activity['duration'] = $webform_submission_data['recheduled_activity_duration'];
-	$activity['status_id'] = \CRM_Core_PseudoConstant::getKey('CRM_Activity_BAO_Activity', 'status_id', 'Scheduled');
-	$activity['source_contact_id'] = $webform_submission_data['civicrm_1_contact_1_contact_existing'];
+        $activity['status_id'] = \CRM_Core_PseudoConstant::getKey('CRM_Activity_BAO_Activity', 'status_id', 'Scheduled');
+        $activity['source_contact_id'] = $webform_submission_data['civicrm_1_contact_1_contact_existing'];
         $newActivity = Activity::create(FALSE)->setValues($activity)->execute();
-	foreach ($activityContacts as $activityContact) {
+        foreach ($activityContacts as $activityContact) {
           ActivityContact::create(FALSE)->addValue('activity_id', $newActivity[0]['id'])->addValue('contact_id', $activityContact['contact_id'])->addValue('record_type_id', $activityContact['record_type_id'])->execute();
         }
       }
