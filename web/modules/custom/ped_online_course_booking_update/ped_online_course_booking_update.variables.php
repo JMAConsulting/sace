889b79aad07998b84834d5d43a79a683ac4cc2cb
Kashish Lathidaida~ <kashish.lathidadia@jmaconsulting.biz>
2023-03-28 15:31:59 -0400

    WIP set defaults for form fields

---
+++ web/modules/custom/ped_online_course_booking_update/ped_online_course_booking_update.variables.php
@@ -0,0 +1,11 @@
+<?php
+
+//Custom Fields
+define('Online_Courses', 'civicrm_1_activity_1_cg2_custom_697');
+define('Youth_Online_Course_Optional_Add_Ons', 'civicrm_1_activity_1_cg2_custom_662');
+define('This_booking_is_for_a_smaller_group_program_department_or_commit','civicrm_1_activity_1_cg2_custom_657');
+define('Preferred_Dates', 'civicrm_1_activity_1_cg2_custom_127');
+define('Number_Participants', 'civicrm_1_activity_1_cg2_custom_24');
+define('Number_of_classes', 'civicrm_1_activity_1_cg2_custom_656');
+define('Grades_and_Ages', 'civicrm_1_activity_1_cg2_custom_25');
+define('Participant_Needs', 'civicrm_1_activity_1_cg2_custom_126');
