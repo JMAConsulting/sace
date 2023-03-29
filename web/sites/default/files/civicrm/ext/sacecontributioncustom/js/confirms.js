805565b02d4578ba3c6e86f7c9804771265ca4ac
Seamus Lee <seamus.lee@jmaconsulting.biz>
2023-03-28 21:44:42 -0400

    Add in CSS and JS for the capital campaign contribution pages

---
+++ web/sites/default/files/civicrm/ext/sacecontributioncustom/js/confirms.js
@@ -0,0 +1,7 @@
+/**
+ * Change Banner Image
+ */
+
+const bannerImage = document.querySelector('.region--hero .block__content .media img')
+bannerImage.src = '/sites/default/files/2023-01/MovingCentre-logo-banner.png';
+bannerImage.dataset.src = '/sites/default/files/2023-01/MovingCentre-logo-banner.png';
