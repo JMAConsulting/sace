b5b1218454006e74661917bd3eccb34073dc8047
Kashish Lathidaida~ <kashish.lathidadia@jmaconsulting.biz>
2023-04-10 20:45:04 -0400

    WIP config sync, webform customizations

--- web/modules/custom/pe_update_booking/js/pe_update_booking.js
+++ web/modules/custom/pe_update_booking/js/pe_update_booking.js
@@ -1,9 +1,13 @@
-jQuery(document).ready(function ($) {
-  if (
-    !$("#edit-civicrm-1-activity-1-cg2-custom-40-customsomethingdifferent").is(
-      ":checked"
-    )
-  ) {
+jQuery(document).ready(function ($, settings) {
+  Drupal.behaviors.pe_update_booking = {
+    attach: function(context, settings) {
+      var Presentation_Topics = settings.pe_update_booking.Presentation_Topics;
+      console.log(Presentation_Topics);
+    }
+  };
+  if (!$("#edit-civicrm-1-activity-1-cg2-custom-40-customsomethingdifferent").is(
+      ":checked")) {
+
     $("#edit-civicrm-1-activity-1-cg2-custom-41").hide();
     $('label[for="edit-civicrm-1-activity-1-cg2-custom-41"]').hide();
   }
@@ -19,6 +23,4 @@ jQuery(document).ready(function ($) {
       }
     }
   );
-
-  $("#edit-civicrm-2-contact-1-fieldset-fieldset").hide(); // Hide organisation section
 });
