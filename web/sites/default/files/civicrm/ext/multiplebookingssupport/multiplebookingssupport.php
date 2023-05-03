34d8766e247ab2d7992f745cddad9aa440d7667d
Seamus Lee <seamuslee001@gmail.com>
2023-05-03 16:42:03 +1000

    Update extension to support apiv3 and add in buildForm and postProcess to support setting the multiple booking flag and create custom module for the views and webform implementation

--- web/sites/default/files/civicrm/ext/multiplebookingssupport/multiplebookingssupport.php
+++ web/sites/default/files/civicrm/ext/multiplebookingssupport/multiplebookingssupport.php
@@ -4,6 +4,8 @@ require_once 'multiplebookingssupport.civix.php';
 // phpcs:disable
 use CRM_Multiplebookingssupport_ExtensionUtil as E;
 // phpcs:enable
+use Civi\Api4\MultipleBooking;
+use Civi\Api4\OptionValue;
 
 /**
  * Implements hook_civicrm_config().
@@ -35,13 +37,43 @@ function multiplebookingssupport_civicrm_enable(): void {
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
- * Implements hook_civicrm_preProcess().
+ * Implements hook_civicrm_buildForm().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
  */
-//function multiplebookingssupport_civicrm_preProcess($formName, &$form): void {
-//
-//}
+function multiplebookingssupport_civicrm_buildForm($formName, &$form): void {
+  if ($formName === 'CRM_Admin_Form_Options' && $form->getVar('_gName') === 'activity_type') {
+    $form->addField('is_multiple_booking', [
+      'entity' => 'multiple_booking',
+      'action' => 'create',
+    ]);
+    if (!empty($form->_id)) {
+      $multipleBooking = MultipleBooking::get(FALSE)
+        ->addJoin('OptionValue AS option_value', 'INNER', ['option_value.value', '=', 'activity_type_id'])
+        ->addWhere('option_value.id', '=', $form->_id)
+        ->execute();
+      if (count($multipleBooking) > 0) {
+        $form->setDefaults(['is_multiple_booking' => $multipleBooking[0]['is_multiple_booking']]);
+      }
+    }
+    CRM_Core_Region::instance('form-bottom')->add([
+      'template' => 'CRM/Admin/Form/ActivityTypeMultipleBooking.tpl',
+    ]);
+  }
+}
+
+/**
+ * Implements hook_civicrm_postProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+function multiplebookingssupport_civicrm_postProcess($formName, &$form): void {
+  if ($formName === 'CRM_Admin_Form_Options' && $form->getVar('_gName') === 'activity_type') {
+    $multipleBookingValue = $form->getSubmittedValue('is_multiple_booking') ?? 0;
+    $optionValue = OptionValue::get(FALSE)->addWhere('id', '=', $form->_id)->execute()->first();
+    MultipleBooking::save(FALSE)->setRecords([['is_multiple_booking' => $multipleBookingValue, 'activity_type_id' => $optionValue['value']]])->setMatch(['activity_type_id'])->execute();
+  }
+}
 
 /**
  * Implements hook_civicrm_navigationMenu().
