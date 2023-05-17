99dd80403a4cad9a10aa767f9fdf4cb614ca4644
Monish Deb <monish.deb@jmaconsulting.biz>
2023-05-17 19:15:20 +0530

    #85zt2442a : External Appointment booking form modifications

---
+++ web/modules/custom/external_appointment_booking/external_appointment_booking.variables.php
@@ -0,0 +1,5 @@
+<?php
+
+
+define('End_Date', 'civicrm_1_activity_1_cg2_custom_661');
+define('Start_Date', 'civicrm_1_activity_1_cg2_custom_816');
---
+++ web/modules/custom/external_appointment_booking/js/view.js
@@ -0,0 +1,46 @@
+(function ($, Drupal, drupalSettings) {
+    $(document).ready(function () {
+
+      // Dynamic Your Organization / School fields
+      field = '.form-item-civicrm-1-contact-1-contact-existing';
+      fieldset = '#edit-civicrm-1-contact-1-fieldset-fieldset';
+      description = '#edit-civicrm-1-contact-1-contact-existing--description';
+      $(`${fieldset}`).hide();
+      $('#new-org').on('click', function (e) {
+          e.preventDefault();
+          $(fieldset).toggle();
+          $(field).toggle();
+      });
+
+      $('#existing-org').on('click', function (e) {
+          $('#new-org').trigger('click');
+      });
+
+      $(`${drupalSettings.external_appointment_booking.start_date}`).on('change', function() {
+        var end = $(`${drupalSettings.external_appointment_booking.end_date}`).val();
+        if (end && $(this).val()) {
+          setDuration($(this).val(), end);
+        }
+      });
+
+      $(`${drupalSettings.external_appointment_booking.end_date}`).on('change', function() {
+        var start = $(`${drupalSettings.external_appointment_booking.start_date}`).val();
+        if (start && $(this).val()) {
+          setDuration(start, $(this).val());
+        }
+      });
+
+      function setDuration(start, end) {
+        start = new Date(start);
+        end = new Date(end);
+        if (start > end) {
+          alert('Start Date cannot be after the End Date');
+          $(`${drupalSettings.external_appointment_booking.end_date}`).val('');
+          $('#edit-civicrm-1-activity-1-activity-duration').val('');
+          return;
+        }
+
+        $('#edit-civicrm-1-activity-1-activity-duration').val((end.getTime() - start.getTime()) / (1000 * 60));
+      }
+    });
+})(jQuery, Drupal, drupalSettings);
