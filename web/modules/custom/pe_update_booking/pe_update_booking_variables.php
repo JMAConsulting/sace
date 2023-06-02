7cead13c2b97e36f557f697961ca30bd981e20d5
Kashish Lathidaida~ <kashish.lathidadia@jmaconsulting.biz>
2023-06-01 20:06:47 -0400

    action qa items, webform config sync

--- web/modules/custom/pe_update_booking/pe_update_booking_variables.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('Presentation_Method', 'civicrm_1_activity_1_cg2_custom_88');
-define('Presentation_Topics', 'civicrm_1_activity_1_cg2_custom_40');
-define('Other_Presentation_Topics', 'civicrm_1_activity_1_cg2_custom_41');
-define('Virtual_Session_Fieldset', 'virtual_session');
