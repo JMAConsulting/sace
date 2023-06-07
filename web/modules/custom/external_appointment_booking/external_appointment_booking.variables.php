e1bd6a4f739eae13bdde0e8a92e46bc00c8bac1b
Kashish Lathidaida~ <kashish.lathidadia@jmaconsulting.biz>
2023-06-07 11:25:35 -0400

    config sync, webform modifications

---
+++ web/modules/custom/external_appointment_booking/external_appointment_booking.variables.php
@@ -0,0 +1,5 @@
+<?php
+
+
+define('EXTERNAL_APPOINTMENT_BOOKING_END_DATE', 'civicrm_1_activity_1_cg2_custom_661');
+define('EXTERNAL_APPOINTMENT_BOOKING_START_DATE', 'civicrm_1_activity_1_cg2_custom_816');
