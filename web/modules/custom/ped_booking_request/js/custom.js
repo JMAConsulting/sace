d917c95f89af1868839299b7ea2d82c57fee3028
Kashish Lathidaida~ <kashish.lathidadia@jmaconsulting.biz>
2023-06-07 19:09:30 -0400

    CE form modifications

--- web/modules/custom/ped_booking_request/js/custom.js
+++ web/modules/custom/ped_booking_request/js/custom.js
@@ -7,18 +7,12 @@ if (bt == 204 || bt == 203 || bt == 201) {
       //Dynamic Your Organization / School fields
       field = '.form-item-civicrm-3-contact-1-contact-existing';
       fieldset = '#edit-civicrm-3-contact-1-fieldset-fieldset';
-      // description = '#edit-civicrm-3-contact-1-contact-existing--description';
-
-      if (bt == 58) {
-     //  $(`${field} > label`).text().replace('Organization', 'School');
-      }
 
       $(`${fieldset}`).hide();
       $('#existing-org').on('click', function (e) {
         e.preventDefault();
         $(`${field}`).show();
         $(`${fieldset}`).hide();
-      //  $('.form-item-civicrm-3-contact-1-contact-existing .token-input-delete-token').trigger('click');
         $(`${field} .token-input-delete-token`).trigger('click');
       });
 
@@ -26,7 +20,6 @@ if (bt == 204 || bt == 203 || bt == 201) {
           e.preventDefault();
           $(`${fieldset}`).show();
           $(`${field}`).hide();
-       //   $('.form-item-civicrm-3-contact-1-contact-existing .token-input-delete-token').trigger('click');
         $(`${field} .token-input-delete-token`).trigger('click');
       });
     });
