388a01b86f765d856344c4556d142dbedad6e5a2
Kashish Lathidaida <kashish.lathidadia@jmaconsulting.biz>
2023-06-14 10:53:19 +0000

    Youth Online Course Booking Request Form and Adult Online Course Booking Request Form customizations

---
+++ web/modules/custom/ped_online_course_booking_request/ped_online_course_booking_request.variables.php
@@ -0,0 +1,11 @@
+<?php
+
+define('PED_ONLINE_COURSE_BOOKING_YOUTH_ACTIVITY', 196);
+define('PED_ONLINE_COURSE_BOOKING_ADULT_ACTIVITY', 199);
+define('PED_ONLINE_COURSE_BOOKING_ONLINE_COURSES', 'civicrm_1_activity_1_cg2_custom_697');
+define('PED_ONLINE_COURSE_BOOKING_ONLINE_COURSES_BLURB', 'online_courses_blurb');
+define('PED_ONLINE_COURSE_BOOKING_YOUTH_OR_ADULT', 'civicrm_1_activity_1_cg2_custom_90');
+define('PED_ONLINE_COURSE_BOOKING_OPTIONAL_ADDONS', 'civicrm_1_activity_1_cg2_custom_662');
+define('PED_ONLINE_COURSE_BOOKING_OPTIONAL_ADDONS_SELECT', 'edit-civicrm-1-activity-1-cg2-custom-662');
+define('PED_ONLINE_COURSE_BOOKING_HIGH_SCHOOL_CHECKBOX', 'edit-civicrm-1-activity-1-cg2-custom-25-high-school-ages-15-18');
+define('PED_ONLINE_COURSE_BOOKING_GRADES_AND_AGES', 'civicrm_1_activity_1_cg2_custom_25');
