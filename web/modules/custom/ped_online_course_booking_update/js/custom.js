ef7251c2476fe2da70fb0549d54ccb776d62b187
Kashish Lathidaida <kashish.lathidadia@jmaconsulting.biz>
2023-06-15 01:34:36 +0000

    Youth Online Course Booking Update Form and Adult Online Course Booking Update Form customisations

---
+++ web/modules/custom/ped_online_course_booking_update/js/custom.js
@@ -0,0 +1,61 @@
+(function ($, Drupal, drupalSettings) {
+    $(document).ready(function () {
+
+      $(`${drupalSettings.ped_online_course_booking_update.start_date}`).on('change', function() {
+        var end = $(`${drupalSettings.ped_online_course_booking_update.end_date}`).val();
+        if (end && $(this).val()) {
+          setDuration($(this).val(), end);
+        }
+      });
+
+      $(`${drupalSettings.ped_online_course_booking_update.end_date}`).on('change', function() {
+        var start = $(`${drupalSettings.ped_online_course_booking_update.start_date}`).val();
+        if (start && $(this).val()) {
+          setDuration(start, $(this).val());
+        }
+      });
+
+      function setDuration(start, end) {
+        start = new Date(start);
+        end = new Date(end);
+        if (start > end) {
+          alert('Start Date cannot be after the End Date');
+          $(`${drupalSettings.ped_online_course_booking_update.end_date}`).val('');
+          $('#edit-civicrm-1-activity-1-activity-duration').val('');
+          return;
+        }
+
+        $('#edit-civicrm-1-activity-1-activity-duration').val((end.getTime() - start.getTime()) / (1000 * 60));
+      }
+
+      let optional_addons_select = document.getElementById(`${drupalSettings.ped_online_course_booking_update.optional_addons_select}`);
+      let option = optional_addons_select.options[1].value;
+      $(`#${drupalSettings.ped_online_course_booking_update.optional_addons_select} option`).each(function() {
+        if ($(this).val() != option && $(this).val() != '') {
+          $(this).hide();
+        }
+      });
+      $(`#${drupalSettings.ped_online_course_booking_update.high_school_checkbox}`).click(function () {
+        if ($(this).is(":checked")) {
+          $(`#${drupalSettings.ped_online_course_booking_update.optional_addons_select} option`).each(function() {
+            if ($(this).val() == option) {
+              $(this).hide();
+            }
+            else {
+              $(this).show();
+            }
+          });
+        }
+        else {
+          $(`#${drupalSettings.ped_online_course_booking_update.optional_addons_select} option`).each(function() {
+            if ($(this).val() != option && $(this).val() != '') {
+              $(this).hide();
+            }
+            else {
+              $(this).show();
+            }
+          });
+        }
+      });
+    });
+})(jQuery, Drupal, drupalSettings);
---
+++ web/modules/custom/ped_online_course_booking_update/ped_online_course_booking_update.variables.php
@@ -0,0 +1,15 @@
+<?php
+
+//Custom Fields
+define('youth_activity', 196);
+define('adult_activity', 199);
+define('online_courses', 'civicrm_1_activity_1_cg2_custom_697');
+define('optional_addons', 'civicrm_1_activity_1_cg2_custom_662');
+define('grades_and_ages', 'civicrm_1_activity_1_cg2_custom_25');
+define('End_Date', 'civicrm_1_activity_1_cg2_custom_661');
+define('Start_Date', 'civicrm_1_activity_1_activity_activity_date_time');
+define('Activity_Duration', 'civicrm_1_activity_1_activity_duration');
+define('Facilitating_Program', 'civicrm_1_activity_1_cg2_custom_332');
+define('Resources_Content', 'civicrm_1_activity_1_cg2_custom_331');
+define('Support_Content', 'civicrm_1_activity_1_cg2_custom_330');
+define('high_school_checkbox', 'edit-civicrm-1-activity-1-cg2-custom-25-high-school-ages-15-18');
