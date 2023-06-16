9571f8d8568853b767c29f36869037acf4674867
Seamus Lee <seamuslee001@gmail.com>
2023-06-16 13:11:10 +1000

    Additional patch missed

--- web/modules/custom/sace_calendar_customisations/js/sace_calendar_booking_calendar_copy.js
+++ web/modules/custom/sace_calendar_customisations/js/sace_calendar_booking_calendar_copy.js
@@ -1,6 +1,6 @@
 (function($) {
   $(window).on('jsframe_create', function(event) {
-    let frame = event.data.jsFrame;
+    let frame = event.jsFrame;
     frame.on('#copy-evaluation-url-button', 'click', function(_frame, e) {
       e.preventDefault();
       navigator.clipboard.writeText($('#evaluation-form-link').prop('href'));
