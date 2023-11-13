cef03061127b99be2e864d4d209d0fe6d6191233
Seamus Lee <seamus.lee@jmaconsulting.biz>
2023-11-12 23:40:13 -0500

    Apply patch to fix php8.1 error

--- vendor/composer/include_paths.php
+++ vendor/composer/include_paths.php
@@ -6,7 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    $vendorDir . '/civicrm/civicrm-core/vendor/tecnickcom',
     $vendorDir . '/pear/archive_tar',
     $vendorDir . '/pear/auth_sasl',
     $vendorDir . '/pear/console_getopt',
@@ -19,4 +18,5 @@ return array(
     $vendorDir . '/pear/pear-core-minimal/src',
     $vendorDir . '/pear/pear_exception',
     $vendorDir . '/pear/validate_finance_creditcard',
+    $vendorDir . '/civicrm/civicrm-core/vendor/tecnickcom',
 );
