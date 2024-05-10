11e1cdc838ec272c3857d6ff09bffcae9dfad8fe
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-10 13:45:15 -0400

    Setting default form values based on aid

--- web/modules/custom/clin_intake_adult_data/clin_intake_adult_data.variables.php
+++ web/modules/custom/clin_intake_adult_data/clin_intake_adult_data.variables.php
@@ -1,14 +1,7 @@
 <?php
 
-define('PE_UPDATE_BOOKING_RESOURCES_CONTENT', 'civicrm_1_activity_1_cg2_custom_331');
-define('PE_UPDATE_BOOKING_SUPPORT_CONTENT', 'civicrm_1_activity_1_cg2_custom_330');
-define('PE_UPDATE_BOOKING_PRESENTATION_METHOD', 'civicrm_1_activity_1_cg2_custom_88');
-define('PE_UPDATE_BOOKING_PRESENTATION_TOPICS', 'civicrm_1_activity_1_cg2_custom_40');
-define('PE_UPDATE_BOOKING_OTHER_PRESENTATION_TOPICS', 'civicrm_1_activity_1_cg2_custom_41');
-define('PE_UPDATE_BOOKING_VIRTUAL_SESSION_FIELDSET', 'virtual_session');
-define('PE_UPDATE_BOOKING_BOOKING_TYPE', 'civicrm_1_activity_1_activity_activity_type_id');
-define('PE_UPDATE_BOOKING_DELIVERY_FORMAT','civicrm_1_activity_1_cg2_custom_88');
-define('PE_UPDATE_BOOKING_GRADES_AND_AGES', 'civicrm_1_activity_1_cg2_custom_25');
+define('CLIN_INTAKE_ADULT_DATA_INITIAL_CALL', 'civicrm_1_activity_1_cg67_custom_1413');
+define('CLIN_INTAKE_ADULT_DATA_INTAKE_NUMBER', 'civicrm_1_activity_1_cg67_custom_14150');
 define('CLIN_INTAKE_BOOKING_ADULT', '77');
 define('CLIN_INTAKE_BOOKING_C_Y', '78');
 define('PED_BOOKING_UPDATE_START_DATE', 'civicrm_1_activity_1_activity_activity_date_time');
