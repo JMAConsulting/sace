7364f5345cdb63501cdf97bfa27e06573042d1b1
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-13 19:50:22 -0400

    Fixing caller population

--- web/modules/custom/clin_intake_adult_data/clin_intake_adult_data.variables.php
+++ web/modules/custom/clin_intake_adult_data/clin_intake_adult_data.variables.php
@@ -2,7 +2,3 @@
 
 define('CLIN_INTAKE_ADULT_DATA_INITIAL_CALL', 'civicrm_1_activity_1_cg67_custom_1413');
 define('CLIN_INTAKE_ADULT_DATA_INTAKE_NUMBER', 'civicrm_1_activity_1_cg67_custom_1415');
-define('CLIN_INTAKE_BOOKING_ADULT', '77');
-define('CLIN_INTAKE_BOOKING_C_Y', '78');
-define('PED_BOOKING_UPDATE_START_DATE', 'civicrm_1_activity_1_activity_activity_date_time');
-define('PED_BOOKING_UPDATE_END_DATE', 'civicrm_1_activity_1_cg2_custom_661');
