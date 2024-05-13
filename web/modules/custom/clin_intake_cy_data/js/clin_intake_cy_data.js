0e6d73226ec0898a2b4748e8465c6f53af574791
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-13 16:35:49 -0400

    Fixing CY Data JS

--- web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
+++ web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
@@ -1,71 +1,15 @@
-jQuery(document).ready(function ($, settings) {
-  //Presentation Topic Custom/SOmething Different toggle text field
-  $(
-    `${drupalSettings.clin_intake_cy_data.start_date}-date, ${drupalSettings.clin_intake_cy_data.start_date}-time, ${drupalSettings.clin_intake_cy_data.end_date}-date, ${drupalSettings.clin_intake_cy_data.end_date}-time`
-  ).on("input", function () {
-    var end = $(`${drupalSettings.clin_intake_cy_data.end_date}-date`).val();
-    var endTime = $(
-      `${drupalSettings.clin_intake_cy_data.end_date}-time`
-    ).val();
-    var start = $(
-      `${drupalSettings.clin_intake_cy_data.start_date}-date`
-    ).val();
-    var startTime = $(
-      `${drupalSettings.clin_intake_cy_data.start_date}-time`
-    ).val();
-    if (end && start) {
-      setDuration(start + " " + startTime, end + " " + endTime, endTime);
-    }
+jQuery(document).ready(function ($) {
+  // Attach change event handlers to mental health checkboxes
+  $("#edit-civicrm-1-contact-1-cg65-custom-1393-8").change(function () {
+    updateReferralSource();
   });
 
-  function setDuration(start, end, endTime) {
-    start = new Date(start);
-    end = new Date(end);
-    var duration = (end.getTime() - start.getTime()) / (1000 * 60);
-    $("#edit-civicrm-1-activity-1-activity-duration").val(duration);
+  function updateReferralSource() {
+    if ($("edit-civicrm-1-contact-1-cg65-custom-1393-8").prop("checked")) {
+      $(".form-item-civicrm-1-contact-1-cg65-custom-1394").show();
+    } else {
+      $(".form-item-civicrm-1-contact-1-cg65-custom-1394").hide();
+    }
   }
-
-  //let checkbox = "#edit-civicrm-1-activity-1-cg2-custom-40-customunsure";
-  //let field = "#edit-civicrm-1-activity-1-cg2-custom-41";
-  //let lable = 'label[for="edit-civicrm-1-activity-1-cg2-custom-41"]';
-  // let bt = $('#edit-civicrm-1-activity-1-activity-activity-type-id').val();
-  // if (!$(`${checkbox}`).is(":checked")) {
-  //   $(`${field}`).hide();
-  //   $(`${lable}`).hide();
-  // }
-
-  // $(`${checkbox}`).click(
-  //   function () {
-  //     if ($(this).is(":checked")) {
-  //       $(`${field}`).show();
-  //       $(`${lable}`).show();
-  //     } else {
-  //       $(`${field}`).hide();
-  //       $(`${lable}`).hide();
-  //     }
-  //   }
-  // );
-
-  // //Dynamic Your Organization / School fields
-  // var field1 = '.form-item-civicrm-2-contact-1-contact-existing';
-  // var fieldset = '#edit-civicrm-2-contact-1-fieldset-fieldset';
-  // var description = '#edit-civicrm-2-contact-1-contact-existing--description';
-  // $(`${field1}`).hide();
-  // $('#existing-org').on('click', function (e) {
-  //   e.preventDefault();
-  //   $(`${field1}`).show();
-  //   $(`${fieldset}`).hide();
-  // });
-
-  // $('#new-org-block').on('click', function (e) {
-  //     e.preventDefault();
-  //     $(`${fieldset}`).show();
-  //     $(`${field1}`).hide();
-  // });
-
-  //   if (bt == 204 || bt == 203) {
-  //     $('div.js-form-type-textfield form-item-civicrm-1-activity-1-cg58-custom-1267').insertAfter($('#edit-flexbox-08'));
-  //   }
-
-  //   $('#block-oliverolocal-fullcalendarblock').insertAfter($('#edit-civicrm-1-activity-1-activity-details-value').parent().parent());
+  updateReferralSource();
 });
