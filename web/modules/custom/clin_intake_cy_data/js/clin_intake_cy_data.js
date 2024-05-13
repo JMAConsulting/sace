092e0d53da3be25db8eae973b7658017fe3da71b
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-13 16:40:01 -0400

    Fixing Jquery selector

--- web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
+++ web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function ($) {
   });
 
   function updateReferralSource() {
-    if ($("edit-civicrm-1-contact-1-cg65-custom-1393-8").prop("checked")) {
+    if ($("#edit-civicrm-1-contact-1-cg65-custom-1393-8").prop("checked")) {
       $(".form-item-civicrm-1-contact-1-cg65-custom-1394").show();
     } else {
       $(".form-item-civicrm-1-contact-1-cg65-custom-1394").hide();
