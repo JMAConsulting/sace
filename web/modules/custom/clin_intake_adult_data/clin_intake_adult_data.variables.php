e48486e30f5403858efd59a517cfdc026bf36fa8
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-13 17:26:03 -0400

    Fixing global variable defn

--- web/modules/custom/clin_intake_adult_data/clin_intake_adult_data.variables.php
+++ web/modules/custom/clin_intake_adult_data/clin_intake_adult_data.variables.php
@@ -1,7 +1,7 @@
 <?php
 
 define('CLIN_INTAKE_ADULT_DATA_INITIAL_CALL', 'civicrm_1_activity_1_cg67_custom_1413');
-define('CLIN_INTAKE_ADULT_DATA_INTAKE_NUMBER', 'civicrm_1_activity_1_cg67_custom_14150');
+define('CLIN_INTAKE_ADULT_DATA_INTAKE_NUMBER', 'civicrm_1_activity_1_cg67_custom_1415');
 define('CLIN_INTAKE_BOOKING_ADULT', '77');
 define('CLIN_INTAKE_BOOKING_C_Y', '78');
 define('PED_BOOKING_UPDATE_START_DATE', 'civicrm_1_activity_1_activity_activity_date_time');
