9abddf5f7b816fe28c88992af3746c9996a55a50
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-13 17:05:02 -0400

    Adding default values to activity

--- web/modules/custom/clin_intake_cy_data/clin_intake_cy_data.variables.php
+++ web/modules/custom/clin_intake_cy_data/clin_intake_cy_data.variables.php
@@ -11,5 +11,7 @@
 // define('PE_UPDATE_BOOKING_GRADES_AND_AGES', 'civicrm_1_activity_1_cg2_custom_25');
 define('CLIN_INTAKE_BOOKING_ADULT', '77');
 define('CLIN_INTAKE_BOOKING_C_Y', '78');
+define('CLIN_INTAKE_CY_DATA_INITIAL_CALL', 'civicrm_1_activity_1_cg67_custom_1413');
+define('CLIN_INTAKE_CY_DATA_INTAKE_NUMBER', 'civicrm_1_activity_1_cg67_custom_1415');
 define('CLIN_INTAKE_CY_DATA_START_DATE', 'civicrm_1_activity_1_activity_activity_date_time');
 define('CLIN_INTAKE_CY_DATA_END_DATE', 'civicrm_1_activity_1_cg2_custom_661');
