bc8ac58468705187ca95fd06e5cf8ef06791f7e6
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-21 11:49:00 -0400

    Updating button val if case has been reported

--- web/modules/custom/clin_booking/js/clin_booking.js
+++ web/modules/custom/clin_booking/js/clin_booking.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function ($) {
   // Change button value to Submit if legal guardian is no
   function updateButtonLabel() {
-    if ($("#edit-are-you-the-legal-guardian-no").is(":checked")) {
+    if (
+      $("#edit-are-you-the-legal-guardian-no").is(":checked") ||
+      $("edit-has-this-been-reported-no").is(":checked")
+    ) {
       $("#edit-actions-submit").val("Submit");
     } else {
       $("#edit-actions-submit").val("Book Intake");
@@ -9,6 +12,8 @@ jQuery(document).ready(function ($) {
   }
 
   $('input[name="are_you_the_legal_guardian"]').change(updateButtonLabel);
+
+  $('input[name="has_this_been_reported_"]').change(updateButtonLabel);
 
   updateButtonLabel();
 });
