ea89807aab2f112ed78c9b6e35f6c0da1a7eb579
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-22 14:31:09 -0400

    Populating guardian fields

--- web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
+++ web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
@@ -17,4 +17,14 @@ jQuery(document).ready(function ($) {
   $("#edit-civicrm-1-contact-1-cg65-custom-1383-0").removeAttr("checked");
   $("#edit-civicrm-1-contact-1-cg65-custom-1410-0").removeAttr("checked");
   $(".form-item-civicrm-1-contact-1-cg65-custom-1384").hide();
+
+  // Populating parent and guardian field
+  var guardian1 = $("#edit-civicrm-1-contact-1-cg21-custom-234").val();
+  var guardian2 = $("#edit-civicrm-1-contact-1-cg23-custom-242").val();
+
+  // Concatenate the values with "&" symbol
+  var concatenatedValue = guardian1 + " & " + guardian2;
+
+  // Set the concatenated value to the third input element
+  $("#edit-civicrm-1-contact-1-cg65-custom-1391").val(concatenatedValue);
 });
