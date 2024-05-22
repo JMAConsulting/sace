bd538f608da127c974a1cc04f8ba6e69048e5af2
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-21 23:37:08 -0400

    Logging submitted data

---
+++ web/modules/custom/clin_booking/src/Plugin/WebformHandler/ClinBookingWebformHandler.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\clin_booking\Plugin\WebformHandler;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Civi\Api4\EntityTag;
+
+/**
+ * Sace CiviCRM Activity Update Handler.
+ *
+ * @WebformHandler(
+ *   id = "clin_booking_handler",
+ *   label = @Translation("CLIN Booking Handler"),
+ *   category = @Translation("CRM"),
+ *   description = @Translation("Webform Handler for fixing the CLIN Intake Booking Form"),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_OPTIONAL,
+ * )
+ */
+class ClinBookingWebformHandler extends WebformHandlerBase {
+
+  /**
+   * The CiviCRM service.
+   *
+   * @var \Drupal\civicrm\Civicrm
+   */
+  protected $civicrm;
+
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  private $database;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->civicrm = $container->get('civicrm');
+    $instance->database = \Drupal::database();
+    return $instance;
+  }
+
+  /**
+   * Process webform submission when it is about to be saved. Called by the following hook:
+   *
+   * @see webform_civicrm_webform_submission_presave
+   *
+   * @param \Drupal\webform\WebformSubmissionInterface $webform_submission
+   */
+  public function preSave(WebformSubmissionInterface $webform_submission) {
+    $webform_submission_data = $webform_submission->getData();
+    if ($webform_submission_data && $webform_submission_data['civicrm_1_activity_1_activity_activity_type_id'] == PED_ONLINE_COURSE_BOOKING_UPDATE_ADULT_ACTIVITY) {
+      $webform_submission_data[PED_ONLINE_COURSE_BOOKING_UPDATE_OPTIONAL_ADDONS] = 'None';
+      $webform_submission->setData($webform_submission_data);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postSave(WebformSubmissionInterface $webform_submission, $update = TRUE) {
+    $this->civicrm->initialize();
+    $webform_submission_data = $webform_submission->getData();
+    $logger = \Drupal::logger('clin_booking');
+    $logger->info('Webform submission data: @data', ['@data' => print_r($webform_submission_data, TRUE)]);
+    // if ($webform_submission_data) {
+      
+    // }
+  }
+
+}
