3ebbd3e73bd3a91b8139066ae04133d01d9b8e6e
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-23 12:54:43 -0400

    adding function to update activity intake number

--- web/modules/custom/clin_booking/src/Plugin/WebformHandler/ClinBookingWebformHandler.php
+++ web/modules/custom/clin_booking/src/Plugin/WebformHandler/ClinBookingWebformHandler.php
@@ -52,13 +52,13 @@ class ClinBookingWebformHandler extends WebformHandlerBase {
    *
    * @param \Drupal\webform\WebformSubmissionInterface $webform_submission
    */
-  // public function preSave(WebformSubmissionInterface $webform_submission) {
-  //   $webform_submission_data = $webform_submission->getData();
-  //   if ($webform_submission_data && $webform_submission_data['are_you_the_legal_guardian'] === 'No' || $webform_submission_data['has_this_been_reported_'] === 'No') {
-  //     $webform_submission_data['civicrm_1_activity_1_activity_activity_type_id'] = 336;
-  //     $webform_submission->setData($webform_submission_data);
-  //   }
-  // }
+  public function preSave(WebformSubmissionInterface $webform_submission) {
+    $webform_submission_data = $webform_submission->getData();
+    if ($webform_submission_data && $webform_submission_data['are_you_the_legal_guardian'] === 'No' || $webform_submission_data['has_this_been_reported_'] === 'No') {
+      $webform_submission_data['civicrm_1_activity_1_activity_activity_type_id'] = 336;
+      $webform_submission->setData($webform_submission_data);
+    }
+  }
 
   /**
    * {@inheritdoc}
@@ -66,29 +66,64 @@ class ClinBookingWebformHandler extends WebformHandlerBase {
   public function postSave(WebformSubmissionInterface $webform_submission, $update = TRUE) {
     $this->civicrm->initialize();
     $webform_submission_data = $webform_submission->getData();
-  //   if ($webform_submission_data) {
-  //     $activityParams = [
-  //       'activity_type_id' => $webform_submission_data['civicrm_1_activity_1_activity_activity_type_id'], 
-  //       'activity_date_time' => $webform_submission_data['civicrm_1_activity_1_activity_activity_date_time'],
-  //     ];
+    if ($webform_submission_data) {
+      $activityParams = [
+        'activity_type_id' => $webform_submission_data['civicrm_1_activity_1_activity_activity_type_id'], 
+        'activity_date_time' => $webform_submission_data['civicrm_1_activity_1_activity_activity_date_time'],
+      ];
+      $existingActivity = civicrm_api4('Activity', 'get', $activityParams);
 
-  //     $dateTime = new DateTime($webform_submission_data['civicrm_1_activity_1_activity_activity_date_time']);
-  //     $year = $dateTime->format('Y');
+      if ($existingActivity) {
+        generateIntakeNumber($existingActivity);
+        civicrm_api4('Activity', 'update', $existingActivity); 
+      }
+    }
+  }
 
-  //     $existingActivity = civicrm_api4('Activity', 'get', $activityParams);
-  //     if ($existingActivity) {
-  //       $activityParams['id'] = $existingActivity[0]['id'];
-  //       $activityParams['CLIN_Adult_Intake_Activity_Data.Intake_Number'] = generateIntakeNumber($year);
-  //     }
 
-  //     $activityResult = civicrm_api4('Activity', 'update', $activityParams);
-  //   }
-   }
-   // private function generateIntakeNumber($year) {
-//   $activityParams = [
-//     'activity_type_id' => $webform_submission_data['civicrm_1_activity_1_activity_activity_type_id'], 
-//   ];
-// }
+  private function generateIntakeNumber(&$existingActivity)
+  {
+    // Get current year
+    $year = date("Y");
+
+    // Update intake number
+    if($existingActivity[0]['activity_type_id'] === 77)
+      $prefix = 'A';
+    else if ($existingActivity[0]['activity_type_id'] === 78)
+      $prefix = 'C';
+    else
+      return;
+
+    $mostRecent = \Civi\Api4\Activity::get(TRUE)
+      ->addSelect('CLIN_Adult_Intake_Activity_Data.Intake_Number')
+      ->addWhere('CLIN_Adult_Intake_Activity_Data.Intake_Number', 'IS NOT NULL')
+      ->addWhere('activity_type_id', '=', $existingActivity[0]['activity_type_id'])
+      ->addOrderBy('id', 'DESC')
+      ->setLimit(1)
+      ->execute()
+      ->first();
+
+      // If no intake numbers for activity type found, start at 001
+      if(empty($mostRecent))
+        $newIntake = '001';
+
+      // Use regular expression to extract the year and the number at the end
+      else if (preg_match('/^(\d{4}A-)(\d{3})$/', $mostRecent['CLIN_Adult_Intake_Activity_Data.Intake_Number'], $matches)) {
+        $mostRecentYear = $matches[1];
+        $lastIntakeNum = $matches[2];
+        
+        if($mostRecentYear === $year)
+        {
+          // Increment intake num by one
+          $newIntake = str_pad((int)$lastIntakeNum + 1, 3, '0', STR_PAD_LEFT);
+        }
+        // If it is the start of a new year? Restart to 001
+        else
+          $newIntake = '001';
+      }
+    // Set intake number
+    $existingActivity[0]['CLIN_Adult_Intake_Activity_Data.Intake_Number'] = $year . $prefix . '-' . $newIntake;
+  }
 }
 
 
