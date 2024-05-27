82b89ea53a81541faef83e083cd9515b2c928ae5
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-05-27 19:37:40 -0400

    Changing params to classify activity as Intake Incomplete

--- web/modules/custom/clin_booking/src/Plugin/WebformHandler/ClinBookingWebformHandler.php
+++ web/modules/custom/clin_booking/src/Plugin/WebformHandler/ClinBookingWebformHandler.php
@@ -44,6 +44,25 @@ class ClinBookingWebformHandler extends WebformHandlerBase {
     return $instance;
   }
 
+  // /**
+  //  * Process webform submission when it is about to be saved. Called by the following hook:
+  //  *
+  //  * @see webform_civicrm_webform_submission_presave
+  //  *
+  //  * @param \Drupal\webform\WebformSubmissionInterface $webform_submission
+  //  */
+  // public function preSave(WebformSubmissionInterface $webform_submission) {
+  //   \Drupal::logger('clin_booking')->debug('presave ran: @data', ['@data' => print_r($webform_submission, TRUE)]);
+  //   $webform_submission_data = $webform_submission->getData();
+    
+  //   if ($webform_submission_data && ((isset($webform_submission_data['are_you_the_legal_guardian']) && $webform_submission_data['are_you_the_legal_guardian'] === 'No') 
+  //   || (isset($webform_submission_data['has_this_been_reported_']) && $webform_submission_data['has_this_been_reported_'] === 'No'))) {
+  //     $webform_submission_data['civicrm_1_activity_1_activity_activity_type_id'] = 336;
+  //     $webform_submission_data['civicrm_1_activity_1_activity_activity_date_time'] = date('Y-m-d H:i:s', time());
+  //     $webform_submission->setData($webform_submission_data);
+  //   }
+  // }
+
   /**
    * {@inheritdoc}
    */
@@ -66,6 +85,7 @@ class ClinBookingWebformHandler extends WebformHandlerBase {
           ->addValue('subject','NEW')
           ->addValue('source_contact_id', 'user_contact_id')
           ->execute();
+        \Drupal::logger('clin_booking')->debug('Creating new activity: @data', ['@data' => print_r($newActivity, TRUE)]);
       }
       else {
         if ($existingActivity['activity_date_time'] === $existingActivity['created_date']) {
@@ -73,6 +93,7 @@ class ClinBookingWebformHandler extends WebformHandlerBase {
             ->addWhere('id', '=', $existingActivity['id'])
             ->addValue('activity_type_id', 336)
             ->execute();
+          \Drupal::logger('clin_booking')->debug('Updatinging old activity: @data', ['@data' => print_r($newActivity, TRUE)]);
         }
         else {
           $intakeNumber = $this->generateIntakeNumber($existingActivity);
