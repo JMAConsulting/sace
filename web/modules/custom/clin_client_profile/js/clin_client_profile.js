f7d30bd8d5c8995c156c5035ced70920c9bb9a06
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-06-11 13:38:32 -0400

    Altering menu links to prepopulate webform

--- web/modules/custom/clin_client_profile/js/clin_client_profile.js
+++ web/modules/custom/clin_client_profile/js/clin_client_profile.js
@@ -1,1 +1,20 @@
-jQuery(document).ready(function ($) {});
+(function ($) {
+  $(document).ready(function () {
+    // Define the URL pattern to match.
+    var urlPattern =
+      /^https:\/\/staging\.sace\.jmaconsulting\.biz\/client-home\/\d+$/;
+    // Get the current URL.
+    var currentUrl = window.location.href;
+
+    // Check if the current URL matches the defined pattern.
+    if (urlPattern.test(currentUrl)) {
+      // Update the href attribute of the anchor element.
+      $(".tabs__link.js-tabs-link.is-active").attr(
+        "href",
+        function (index, oldHref) {
+          return oldHref + "?cid=" + oldHref.match(/\d+$/)[0];
+        }
+      );
+    }
+  });
+})(jQuery);
