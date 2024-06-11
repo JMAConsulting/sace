839fe0d9c9f0ff3d7747b16d1b34b5a274f75876
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-06-11 19:57:42 -0400

    Updating security form href

--- web/modules/custom/clin_client_profile/js/clin_client_profile.js
+++ web/modules/custom/clin_client_profile/js/clin_client_profile.js
@@ -7,13 +7,19 @@
     var urlPattern = /\/client-home(?:\/\w+)*\/(\d+)/;
     if (urlPattern.test(currentUrl)) {
       var cid = currentUrl.match(urlPattern)[1];
-      // Update the href of the contact info tab
+      // Update the href of form tabs
       $(".tabs__link").each(function () {
         var $this = $(this);
         var href = $this.attr("href");
+        // Filter contact info form by cid
         if (href.indexOf("/client-home/contact-information/" + cid) !== -1) {
           var newHref =
             "/client-home/contact-information/" + cid + "?cid=" + cid;
+          $this.attr("href", newHref);
+        }
+        // Filter security form by cid
+        if (href.indexOf("/client-home/security/" + cid) !== -1) {
+          var newHref = "/client-home/security/" + cid + "?cid=" + cid;
           $this.attr("href", newHref);
         }
       });
