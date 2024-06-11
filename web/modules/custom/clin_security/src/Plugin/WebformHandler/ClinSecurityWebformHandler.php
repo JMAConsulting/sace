2e5fb9af53305f79242524b9d7c74aae06579de0
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-06-11 19:33:25 -0400

    Adding module to handle security tab of client profile

---
+++ web/modules/custom/clin_security/src/Plugin/WebformHandler/ClinSecurityWebformHandler.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\clin_security\Plugin\WebformHandler;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Sace CiviCRM Activity Update Handler.
+ *
+ * @WebformHandler(
+ *   id = "clin_security_handler",
+ *   label = @Translation("CLIN Security Handler"),
+ *   category = @Translation("CRM"),
+ *   description = @Translation("Webform Handler for fixing the CLIN Security Form"),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_OPTIONAL,
+ * )
+ */
+class ClinSecurityWebformHandler extends WebformHandlerBase {
+
+  /**
+   * The CiviCRM service.
+   *
+   * @var \Drupal\civicrm\Civicrm
+   */
+  protected $civicrm;
+
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  private $database;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->civicrm = $container->get('civicrm');
+    $instance->database = \Drupal::database();
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postSave(WebformSubmissionInterface $webform_submission, $update = TRUE) {
+    $this->civicrm->initialize();
+    $webform_submission_data = $webform_submission->getData();
+    if ($webform_submission_data) {
+        
+    }
+  }
+}
