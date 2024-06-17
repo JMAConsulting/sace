d1a9b35cec238c12c6800a99c6c1d4499f8d7c8c
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-06-17 14:04:26 -0400

    Freezing date fields

--- web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
+++ web/modules/custom/clin_intake_cy_data/js/clin_intake_cy_data.js
@@ -135,4 +135,13 @@ jQuery(document).ready(function ($) {
 
   convertDateFormat("#edit-civicrm-1-activity-1-cg67-custom-1414");
   convertDateFormat("#edit-civicrm-1-activity-1-cg67-custom-1413");
+
+  function freezeDates(fieldName) {
+    $(fieldName + "-date").prop("disabled", true);
+    $(fieldName + "-time").prop("disabled", true);
+    fieldName.addClass("webform-readonly");
+  }
+
+  freezeDates("#edit-civicrm-1-activity-1-cg67-custom-1413");
+  freezeDates("#edit-civicrm-1-activity-1-cg67-custom-1414");
 });
