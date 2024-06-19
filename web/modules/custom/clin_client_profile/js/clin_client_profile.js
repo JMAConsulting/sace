ffb6c668bec2179edd5eab729739c87e9e6b0155
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-06-19 15:21:34 -0400

    Fixing formatting of JS

--- web/modules/custom/clin_client_profile/js/clin_client_profile.js
+++ web/modules/custom/clin_client_profile/js/clin_client_profile.js
@@ -35,21 +35,25 @@
         }
       });
     }
-if(urlPattern2.test(currentUrl)) {
-	  $('tr').each(function() {
-    // Find the cell with the activity type ID
-    var activityTypeCell = $(this).find('.views-field-activity-type-id > div');
+  if (urlPattern2.test(currentUrl)) {
+    $("tr").each(function () {
+      // Find the cell with the activity type ID
+      var activityTypeCell = $(this).find(
+        ".views-field-activity-type-id > div"
+      );
 
-    if (activityTypeCell.length) {
-      // Get the class name (assuming there is only one class and it represents the color)
-      var className = activityTypeCell.attr('class');
+      if (activityTypeCell.length) {
+        // Get the class name (assuming there is only one class and it represents the color)
+        var className = activityTypeCell.attr("class");
 
-      // Apply the class as the background color of the first cell in the row
-      if (className) {
-        $(this).find('td:first').css('background-color', '#' + className);
+        // Apply the class as the background color of the first cell in the row
+        if (className) {
+          $(this)
+            .find("td:first")
+            .css("background-color", "#" + className);
+        }
       }
-    }
-  });
-}
+    });
+  }
   });
 })(jQuery);
