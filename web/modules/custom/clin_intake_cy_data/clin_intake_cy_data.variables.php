14dcae8125863d03009ca79e92a23b099d8f24d9
laurensmillie <seamus.lee@jmaconsulting.biz>
2024-06-20 12:30:06 -0400

    Fixing bday prepopulation

--- web/modules/custom/clin_intake_cy_data/clin_intake_cy_data.variables.php
+++ web/modules/custom/clin_intake_cy_data/clin_intake_cy_data.variables.php
@@ -3,3 +3,4 @@
 define('CLIN_INTAKE_CY_DATA_INITIAL_CALL', 'civicrm_1_activity_1_cg67_custom_1413');
 define('CLIN_INTAKE_CY_DATA_INTAKE_NUMBER', 'civicrm_1_activity_1_cg67_custom_1415');
 define('CLIN_INTAKE_CY_DATA_CALLER_NAME', 'civicrm_1_contact_1_cg65_custom_1389');
+define('CLIN_INTAKE_CY_DATA_BDAY','birth_date');
