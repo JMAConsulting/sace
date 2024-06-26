6adc17818f178d9673075a39a2551ad010afc881
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-06-26 01:09:25 -0400

    Adding JQuery call

--- web/modules/custom/clin_client_profile/js/clin_edit_contact.js
+++ web/modules/custom/clin_client_profile/js/clin_edit_contact.js
@@ -1,6 +1,10 @@
-if ($(".webform-locked-message").length) {
-  $("#webform-submission-client-profile-data-add-form div").addClass(
-    "webform-readonly"
-  );
-  $("#edit-submit").hide();
-}
+(function ($) {
+  $(document).ready(function () {
+    if ($(".webform-locked-message").length) {
+      $("#webform-submission-client-profile-data-add-form div").addClass(
+        "webform-readonly"
+      );
+      $("#edit-submit").hide();
+    }
+  });
+})(jQuery);
