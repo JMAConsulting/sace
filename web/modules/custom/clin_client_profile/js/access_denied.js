b596b3075daeb6a1af720861418d41b29ab53911
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-07-04 18:23:34 -0400

    Removing add note to appointment buttons if perms are restricted

--- web/modules/custom/clin_client_profile/js/access_denied.js
+++ web/modules/custom/clin_client_profile/js/access_denied.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function ($) {
   var currentUrl = window.location.href;
   var editContactInfo = /\/contact-information\/edit\/(\d+)/;
   var flags = /\/contact-information\/flags\/(\d+)/;
+  var appointments = /\/contact-information\/appointments\/(\d+)/;
 
   if (editContactInfo.test(currentUrl)) {
     $("#webform-submission-client-profile-add-form div").addClass(
@@ -9,6 +10,8 @@ jQuery(document).ready(function ($) {
     );
     $("#edit-submit").hide();
   } else if (flags.test(currentUrl)) {
+    $(".meeting-buttons .button--secondary").remove();
+  } else if (appointments.test(currentUrl)) {
     $(".meeting-buttons .button--secondary").remove();
   }
 });
