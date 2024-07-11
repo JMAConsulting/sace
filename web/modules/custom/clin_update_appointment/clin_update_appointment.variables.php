7a9195755f1b8e735469847e89a0a6ac85f91feb
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-07-11 18:41:04 -0400

    Do not prepopulate details

--- web/modules/custom/clin_update_appointment/clin_update_appointment.variables.php
+++ web/modules/custom/clin_update_appointment/clin_update_appointment.variables.php
@@ -1,6 +1,5 @@
 <?php
 
 define('CLIN_UPDATE_APP_ACTIVITY_TYPE', 'civicrm_1_activity_1_activity_activity_type_id');
-define('CLIN_UPDATE_APP_DETAILS', 'civicrm_1_activity_1_activity_details');
 define('CLIN_UPDATE_APP_DURATION', 'civicrm_1_activity_1_activity_duration');
 define('CLIN_UPDATE_APP_ASSIGNEE_ID','civicrm_3_contact_1_contact_existing');
