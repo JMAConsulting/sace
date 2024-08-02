cf5c039e2088cb22ff3b4bc067c3978866df9e21
Monish Deb <monish.deb@jmaconsulting.biz>
2024-08-02 10:26:12 -0400

    Add separate youth_online_course_booking_request webform and custom module

---
+++ web/modules/custom/youth_online_course_booking_request/youth_online_course_booking_request.variables.php
@@ -0,0 +1,7 @@
+<?php
+
+define('YOUTH_ONLINE_COURSE_BOOKING_YOUTH_OR_ADULT', 'civicrm_1_activity_1_cg2_custom_90');
+define('YOUTH_ONLINE_COURSE_BOOKING_OPTIONAL_ADDONS', 'civicrm_1_activity_1_cg2_custom_662');
+define('YOUTH_ONLINE_COURSE_BOOKING_OPTIONAL_ADDONS_SELECT', 'edit-civicrm-1-activity-1-cg2-custom-662');
+define('YOUTH_ONLINE_COURSE_BOOKING_HIGH_SCHOOL_CHECKBOX', 'edit-civicrm-1-activity-1-cg2-custom-25-high-school-ages-15-18');
+define('YOUTH_ONLINE_COURSE_BOOKING_GRADES_AND_AGES', 'civicrm_1_activity_1_cg2_custom_25');
