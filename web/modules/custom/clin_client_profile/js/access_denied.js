4eda34d43d5da36aa95afb2c44dc0515f21d93d2
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-08-14 15:48:32 -0400

    Handling edit form permissions on edit contact info webform

--- web/modules/custom/clin_client_profile/js/access_denied.js
+++ web/modules/custom/clin_client_profile/js/access_denied.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function ($) {
   var currentUrl = window.location.href;
-  var editContactInfo = /\/contact-information\/edit\/(\d+)/;
+  var contactInfo = /\/contact-information\/(\d+)/;
   var flags = /\/contact-information\/flags\/(\d+)/;
   var appointments = /\/contact-information\/appointments\/(\d+)/;
 
-  if (editContactInfo.test(currentUrl)) {
+  if (contactInfo.test(currentUrl)) {
     $("#webform-submission-client-profile-add-form div").addClass(
       "webform-readonly"
     );
