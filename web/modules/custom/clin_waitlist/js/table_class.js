43c18f386cecd2826a36c75f447cf9c8a0195951
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-08-16 13:34:49 -0400

    Fix jquery syntax

--- web/modules/custom/clin_waitlist/js/table_class.js
+++ web/modules/custom/clin_waitlist/js/table_class.js
@@ -1,2 +1,4 @@
 // Replace table class from views aggregator plus with default views-table class
-$('.table').addClass('views-table').removeClass('table');
+$(document).ready(function(){
+    $('.table').addClass('views-table').removeClass('table');
+});
