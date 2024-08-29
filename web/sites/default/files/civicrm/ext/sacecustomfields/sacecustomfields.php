5704e59f1f03009f05061dd7859f49209d44de9f
LaurenSmillie <lauren.smillie@jmaconsulting.biz>
2024-08-29 09:39:18 -0400

    Adding ext to import custom fields

---
+++ web/sites/default/files/civicrm/ext/sacecustomfields/sacecustomfields.php
@@ -0,0 +1,98 @@
+<?php
+
+require_once 'sacecustomfields.civix.php';
+// phpcs:disable
+use CRM_Sacecustomfields_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function sacecustomfields_civicrm_config(&$config): void {
+  _sacecustomfields_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function sacecustomfields_civicrm_install(): void {
+  _sacecustomfields_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function sacecustomfields_civicrm_postInstall(): void {
+  _sacecustomfields_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function sacecustomfields_civicrm_uninstall(): void {
+  _sacecustomfields_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function sacecustomfields_civicrm_enable(): void {
+  _sacecustomfields_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function sacecustomfields_civicrm_disable(): void {
+  _sacecustomfields_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function sacecustomfields_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _sacecustomfields_civix_civicrm_upgrade($op, $queue);
+}
+
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function sacecustomfields_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function sacecustomfields_civicrm_navigationMenu(&$menu): void {
+//  _sacecustomfields_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _sacecustomfields_civix_navigationMenu($menu);
+//}
