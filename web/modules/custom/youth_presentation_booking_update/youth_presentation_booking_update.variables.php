ae74c105eba896e0ae2f1febaf6b47fb6a402210
Monish Deb <monish.deb@jmaconsulting.biz>
2024-09-07 14:53:14 +0530

    Add youth_presentation_booking_update module

---
+++ web/modules/custom/youth_presentation_booking_update/youth_presentation_booking_update.variables.php
@@ -0,0 +1,4 @@
+<?php
+
+define('YOUTH_BOOKING_UPDATE_START_DATE', 'civicrm_1_activity_1_activity_activity_date_time');
+define('YOUTH_BOOKING_UPDATE_END_DATE', 'civicrm_1_activity_1_cg2_custom_661');
