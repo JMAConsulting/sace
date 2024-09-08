fee1f75fbdba9c4d302f627baccf980d4de10842
Monish Deb <monish.deb@jmaconsulting.biz>
2024-09-08 13:54:33 +0530

    Add institutional_support_booking_update module

---
+++ web/modules/custom/institutional_support_booking_update/institutional_support_booking_update.variables.php
@@ -0,0 +1,4 @@
+<?php
+
+define('INSTITUTIONAL_SUPPORT_BOOKING_UPDATE_START_DATE', 'civicrm_1_activity_1_activity_activity_date_time');
+define('INSTITUTIONAL_SUPPORT_BOOKING_UPDATE_END_DATE', 'civicrm_1_activity_1_cg2_custom_661');
