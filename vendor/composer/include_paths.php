cc9901ecb164f28659499dbf111b17e3a7019716
Seamus Lee <seamuslee001@gmail.com>
2025-01-09 18:49:26 +1100

    Update Blazy and Slick modules to supported versions

--- vendor/composer/include_paths.php
+++ vendor/composer/include_paths.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     $vendorDir . '/civicrm/civicrm-core/vendor/tecnickcom',
+    $vendorDir . '/pear/archive_tar',
     $vendorDir . '/pear/auth_sasl',
     $vendorDir . '/pear/console_getopt',
     $vendorDir . '/pear/db',
@@ -18,5 +19,4 @@ return array(
     $vendorDir . '/pear/pear-core-minimal/src',
     $vendorDir . '/pear/pear_exception',
     $vendorDir . '/pear/validate_finance_creditcard',
-    $vendorDir . '/pear/archive_tar',
 );
