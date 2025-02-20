af808079e1385ec7fea19e935de47578cc0b6b34
Seamus Lee <seamuslee001@gmail.com>
2025-02-20 16:38:18 +1100

    Drupal Security Upgrade 10.3.13

--- vendor/composer/include_paths.php
+++ vendor/composer/include_paths.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     $vendorDir . '/civicrm/civicrm-core/vendor/tecnickcom',
+    $vendorDir . '/pear/archive_tar',
     $vendorDir . '/pear/auth_sasl',
     $vendorDir . '/pear/console_getopt',
     $vendorDir . '/pear/db',
@@ -15,8 +16,7 @@ return array(
     $vendorDir . '/pear/mail_mime',
     $vendorDir . '/pear/net_smtp',
     $vendorDir . '/pear/net_socket',
-    $vendorDir . '/pear/pear-core-minimal/src',
     $vendorDir . '/pear/pear_exception',
     $vendorDir . '/pear/validate_finance_creditcard',
-    $vendorDir . '/pear/archive_tar',
+    $vendorDir . '/pear/pear-core-minimal/src',
 );
