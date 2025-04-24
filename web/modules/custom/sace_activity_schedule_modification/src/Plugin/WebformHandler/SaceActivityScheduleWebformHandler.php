d731491af2ac910cc117f1cf4545f39402b2a113
benjamin <ben.walpole@jmaconsulting.biz>
2025-04-24 13:30:14 +0100

    fix indentation

--- web/modules/custom/sace_activity_schedule_modification/src/Plugin/WebformHandler/SaceActivityScheduleWebformHandler.php
+++ web/modules/custom/sace_activity_schedule_modification/src/Plugin/WebformHandler/SaceActivityScheduleWebformHandler.php
@@ -79,41 +79,41 @@ class SaceActivityScheduleWebformHandler extends WebformHandlerBase {
     ]);
     if ($webform_submission_data && $civicrm_submission_data) {
       while ($row = $civicrm_submission_data->fetchAssoc()) {
-      $contacts = [];
-      $data = unserialize($row['civicrm_data']);
-      if (!empty($webform_submission_data['staff'])) {
-        $contacts = array_merge($contacts, $webform_submission_data['staff']);
-      }
-      if (!empty($webform_submission_data['organisation_select'])) {
-        $contacts = array_merge($contacts, $webform_submission_data['organisation_select']);
-      }
-      if (!empty($webform_submission_data['user_team'])) {
-        $user_ids = array_keys(\Drupal::entityTypeManager()->getStorage('user')->loadByProperties(['field_user_team' => $webform_submission_data['user_team']]));
-        $team_contact_ids = UFMatch::get(FALSE)->addWhere('uf_id', 'IN', $user_ids)->addWhere('contact_id.contact_sub_type', '!=', 'Former_Staff')->execute()->column('contact_id');
-        $contacts = array_merge($contacts, $team_contact_ids);
-        $user_team = implode(',', $webform_submission_data['user_team']);
-        Activity::update(FALSE)
-          ->addWhere('id', '=', $data['activity'][1]['id'])
-          ->addValue('CE_External_Activities.User_Team_filter', $user_team)
-          ->execute();
-      }
-      if (!empty($contacts)) {
-        $activityContacts = \Civi\Api4\ActivityContact::get(FALSE)
-          ->addWhere('activity_id', '=', $data['activity'][1]['id'])
-          ->addWhere('record_type_id:name', '=', 'Activity Assignees')
-          ->execute();
-        foreach ($activityContacts as $activityContact) {
-          \Civi\Api4\ActivityContact::delete(FALSE)->addWhere('id', '=', $activityContact['id'])->execute();
+        $contacts = [];
+        $data = unserialize($row['civicrm_data']);
+        if (!empty($webform_submission_data['staff'])) {
+          $contacts = array_merge($contacts, $webform_submission_data['staff']);
         }
-        $unique_contacts = array_unique($contacts);
-        foreach ($unique_contacts as $contact_id) {
-          ActivityContact::create(FALSE)
-            ->addValue('contact_id', $contact_id)
-            ->addValue('activity_id', $data['activity'][1]['id'])
-            ->addValue('record_type_id:name', 'Activity Assignees')
+        if (!empty($webform_submission_data['organisation_select'])) {
+          $contacts = array_merge($contacts, $webform_submission_data['organisation_select']);
+        }
+        if (!empty($webform_submission_data['user_team'])) {
+          $user_ids = array_keys(\Drupal::entityTypeManager()->getStorage('user')->loadByProperties(['field_user_team' => $webform_submission_data['user_team']]));
+          $team_contact_ids = UFMatch::get(FALSE)->addWhere('uf_id', 'IN', $user_ids)->addWhere('contact_id.contact_sub_type', '!=', 'Former_Staff')->execute()->column('contact_id');
+          $contacts = array_merge($contacts, $team_contact_ids);
+          $user_team = implode(',', $webform_submission_data['user_team']);
+          Activity::update(FALSE)
+            ->addWhere('id', '=', $data['activity'][1]['id'])
+            ->addValue('CE_External_Activities.User_Team_filter', $user_team)
             ->execute();
         }
-      }
+        if (!empty($contacts)) {
+          $activityContacts = \Civi\Api4\ActivityContact::get(FALSE)
+            ->addWhere('activity_id', '=', $data['activity'][1]['id'])
+            ->addWhere('record_type_id:name', '=', 'Activity Assignees')
+            ->execute();
+          foreach ($activityContacts as $activityContact) {
+            \Civi\Api4\ActivityContact::delete(FALSE)->addWhere('id', '=', $activityContact['id'])->execute();
+          }
+          $unique_contacts = array_unique($contacts);
+          foreach ($unique_contacts as $contact_id) {
+            ActivityContact::create(FALSE)
+              ->addValue('contact_id', $contact_id)
+              ->addValue('activity_id', $data['activity'][1]['id'])
+              ->addValue('record_type_id:name', 'Activity Assignees')
+              ->execute();
+          }
+        }
       }
     }
   }
