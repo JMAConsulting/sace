9f876575eabe863fd796be5bd91b71513b29e58b
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2025-07-23 10:49:12 +0530

    Added location to description

--- web/sites/default/files/civicrm/ext/sace/Civi/SACE/CalendarFeed.php
+++ web/sites/default/files/civicrm/ext/sace/Civi/SACE/CalendarFeed.php
@@ -71,6 +71,7 @@ class CalendarFeed extends AutoSubscriber {
       if ($address) {
         unset($address['id']);
         $e->row['address_location'] = implode(", ", $address);
+        $e->row['description'] .= '<p>Location: </p>' . $implode(", ", $address);
       }
     }
   }
