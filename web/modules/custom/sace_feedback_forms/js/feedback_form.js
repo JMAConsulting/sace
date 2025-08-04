546f26f1752b63374dde04d246b095dc31168fd4
Monish Deb <monish.deb@jmaconsulting.biz>
2025-08-04 05:24:46 -0400

    QA fixes on feedback form

--- web/modules/custom/sace_feedback_forms/js/feedback_form.js
+++ web/modules/custom/sace_feedback_forms/js/feedback_form.js
@@ -1,4 +1,4 @@
 (function ($, Drupal, drupalSettings) {
-  $('#edit-date-of-presentation').parent().insertAfter($("#edit-civicrm-1-activity-1-activity-activity-date-time").parent());
+  $('#edit-date-of-presentation').parent().insertAfter($("#edit-civicrm-1-activity-1-cg60-custom-1296").parent());
 })(jQuery, Drupal, drupalSettings);
 
