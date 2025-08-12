9d6f9d711b46aa846ffb5df0d6c9dc06f7f2324b
Seamus Lee <seamuslee001@gmail.com>
2025-08-12 16:17:22 +1000

    Update Import Helper extension to the latest from git repo

--- web/sites/default/files/civicrm/ext/uk.artfulrobot.civicrm.importhelper/importhelper.php
+++ web/sites/default/files/civicrm/ext/uk.artfulrobot.civicrm.importhelper/importhelper.php
@@ -44,7 +44,6 @@ function importhelper_civicrm_install() {
 function importhelper_civicrm_uninstall() {
   // Remove our table.
   $dao = CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS `civicrm_csv_match_cache`;");
-  _importhelper_civix_civicrm_uninstall();
 }
 
 /**
@@ -54,31 +53,6 @@ function importhelper_civicrm_uninstall() {
  */
 function importhelper_civicrm_enable() {
   _importhelper_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function importhelper_civicrm_disable() {
-  _importhelper_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function importhelper_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _importhelper_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -120,22 +94,4 @@ function importhelper_civicrm_navigationMenu(&$menu) {
  */
 function importhelper_civicrm_alterAPIPermissions($entity, $action, &$params, &$permissions) {
   $permissions['csv_helper']['default'] = ['access CiviCRM', 'view all contacts'];
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function importhelper_civicrm_postInstall() {
-  _importhelper_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function importhelper_civicrm_entityTypes(&$entityTypes) {
-  _importhelper_civix_civicrm_entityTypes($entityTypes);
 }
